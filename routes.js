b1acde4c214e781278e49b2d0970330555c81c5f
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-05-20 02:34:17 +0530

    Showcase

---
+++ routes.js
@@ -0,0 +1,27 @@
+import { products } from './data'
+import { Router } from '@layer0/core'
+
+export default new Router()
+  .get('/images/:path*', ({ cache, serveStatic }) => {
+    cache({ edge: { maxAgeSeconds: 60 * 60 * 24 * 365 }, browser: false })
+    serveStatic('images/:path*')
+  })
+  .get('/products/all', ({ compute, cache }) => {
+    cache({ edge: { maxAgeSeconds: 60 * 60 * 24 * 365 }, browser: false })
+    compute((req, res) => {
+      res.setHeader('content-type', 'application/json')
+      res.body = JSON.stringify(products)
+      res.statusCode = 200
+      res.statusMessage = 'OK'
+    })
+  })
+  .get('/products/:slug', ({ compute, cache }) => {
+    cache({ edge: { maxAgeSeconds: 60 * 60 * 24 * 365 }, browser: false })
+    compute((req, res) => {
+      const { slug } = req.params
+      res.setHeader('content-type', 'application/json')
+      res.body = JSON.stringify(products.find((i) => i.slug === slug))
+      res.statusCode = 200
+      res.statusMessage = 'OK'
+    })
+  })
