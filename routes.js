eae124493357b7a363f44bc4a9cd674691617f56
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-05-21 00:24:06 +0530

    Allow CORS

--- routes.js
+++ routes.js
@@ -10,6 +10,9 @@ export default new Router()
     cache({ edge: { maxAgeSeconds: 60 * 60 * 24 * 365 }, browser: false })
     compute((req, res) => {
       res.setHeader('content-type', 'application/json')
+      res.setHeader('Access-Control-Allow-Origin', '*')
+      res.setHeader('Access-Control-Allow-Methods', 'GET')
+      res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
       res.body = JSON.stringify(products)
       res.statusCode = 200
       res.statusMessage = 'OK'
@@ -20,6 +23,9 @@ export default new Router()
     compute((req, res) => {
       const { slug } = req.params
       res.setHeader('content-type', 'application/json')
+      res.setHeader('Access-Control-Allow-Origin', '*')
+      res.setHeader('Access-Control-Allow-Methods', 'GET')
+      res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
       res.body = JSON.stringify(products.find((i) => i.slug === slug))
       res.statusCode = 200
       res.statusMessage = 'OK'
