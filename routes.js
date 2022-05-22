e4b84eae6cba51d3d4475e79aac45110fcaa86fb
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2022-05-23 00:11:40 +0530

    Add product-images route for images

--- routes.js
+++ routes.js
@@ -1,6 +1,12 @@
 import { products } from './data'
 import { Router } from '@layer0/core'
 
+const CORS= (res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+}
+
 export default new Router()
   .get('/images/:path*', ({ cache, serveStatic }) => {
     cache({ edge: { maxAgeSeconds: 60 * 60 * 24 * 365 }, browser: false })
@@ -10,9 +16,7 @@ export default new Router()
     cache({ edge: { maxAgeSeconds: 60 * 60 * 24 * 365 }, browser: false })
     compute((req, res) => {
       res.setHeader('content-type', 'application/json')
-      res.setHeader('Access-Control-Allow-Origin', '*')
-      res.setHeader('Access-Control-Allow-Methods', 'GET')
-      res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+      CORS(res)
       res.body = JSON.stringify(products)
       res.statusCode = 200
       res.statusMessage = 'OK'
@@ -23,10 +27,20 @@ export default new Router()
     compute((req, res) => {
       const { slug } = req.params
       res.setHeader('content-type', 'application/json')
-      res.setHeader('Access-Control-Allow-Origin', '*')
-      res.setHeader('Access-Control-Allow-Methods', 'GET')
-      res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+      CORS(res)
       res.body = JSON.stringify(products.find((i) => i.slug === slug))
+      res.statusCode = 200
+      res.statusMessage = 'OK'
+    })
+  })
+  .get('/product-images/:slug', ({ compute, cache }) => {
+    cache({ edge: { maxAgeSeconds: 60 * 60 * 24 * 365 }, browser: false })
+    compute((req, res) => {
+      const { slug } = req.params
+      res.setHeader('content-type', 'application/json')
+      CORS(res)
+      const item = JSON.stringify(products.find((i) => i.slug === slug))
+      res.body = item.images
       res.statusCode = 200
       res.statusMessage = 'OK'
     })
