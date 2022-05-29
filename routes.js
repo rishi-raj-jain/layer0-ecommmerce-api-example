f4660edfd0a9e9c02ca50d65e62b7bf74eb138c8
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2022-05-29 12:55:08 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,30 +1,27 @@
 import { products } from './data'
 import { Router } from '@layer0/core'
 
-const CORS= (res, setHeader) => {
+const CORS= (res) => {
     if (res){
         res.setHeader('Access-Control-Allow-Origin', '*')
         res.setHeader('Access-Control-Allow-Methods', 'GET')
         res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    }
-    else {
-        setHeader('Access-Control-Allow-Origin', '*')
-        setHeader('Access-Control-Allow-Methods', 'GET')
-        setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     }
 }
 
 export default new Router()
   .get('/images/:path*', ({ cache, serveStatic, setHeader }) => {
     cache({ edge: { maxAgeSeconds: 60 * 60 * 24 * 365 }, browser: false })
-    CORS(undefined, setHeader)
+    setHeader('Access-Control-Allow-Origin', '*')
+    setHeader('Access-Control-Allow-Methods', 'GET')
+    setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     serveStatic('images/:path*')
   })
   .get('/products/all', ({ compute, cache }) => {
     cache({ edge: { maxAgeSeconds: 60 * 60 * 24 * 365 }, browser: false })
     compute((req, res) => {
       res.setHeader('content-type', 'application/json')
-      CORS(res, undefined)
+      CORS(res)
       res.body = JSON.stringify(products)
       res.statusCode = 200
       res.statusMessage = 'OK'
@@ -35,7 +32,7 @@ export default new Router()
     compute((req, res) => {
       const { slug } = req.params
       res.setHeader('content-type', 'application/json')
-      CORS(res, undefined)
+      CORS(res)
       res.body = JSON.stringify(products.find((i) => i.slug === slug))
       res.statusCode = 200
       res.statusMessage = 'OK'
@@ -46,7 +43,7 @@ export default new Router()
     compute((req, res) => {
       const { slug } = req.params
       res.setHeader('content-type', 'application/json')
-      CORS(res, undefined)
+      CORS(res)
       const item = products.find((i) => i.slug === slug)
       res.body = JSON.stringify(item.images)
       res.statusCode = 200
